d418924d99cf2077fbbf2d3b6cce100bb663c586
jamieroberts <jamie.roberts1111@gmail.com>
2020-12-16 21:26:55 +0000

    Menu ordering.

--- src/main/java/com/manorbuttys/api/menu/controller/MenuController.java
+++ src/main/java/com/manorbuttys/api/menu/controller/MenuController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MenuController {
@@ -18,6 +25,10 @@ public class MenuController {
 
     @GetMapping(value = "/menu", produces = "application/json")
     public Iterable<MenuSection> getMenu() {
-        return menuRepository.findAll();
+        List<MenuSection> menu = (List<MenuSection>) menuRepository.findAll();
+        Stream<MenuSection> sorted = menu.stream()
+                .sorted();
+
+        return sorted.collect(Collectors.toList());
     }
 }
